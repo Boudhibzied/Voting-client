b5814e3213f7726eda3036b3464649d785a936e7
boudhibzied <zied.boudhib@esprit.tn>
2017-01-15 03:06:53 +0100

    unit test the reducer & implement the logic adding redux lib

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+import {Map} from 'immutable';
+
+const setState = (state, newState) =>{
+  return state.merge(newState);
+}
+
+const reducer = (state = Map(), action) =>{
+  switch (action.type) {
+  case 'SET_STATE':
+    return setState(state, action.state);
+  }
+  return state;
+}
+
+export default reducer;
---
+++ test/reducer_spec.js
@@ -0,0 +1,68 @@
+import {List, Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+  it('handles SET_STATE', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: Map({
+        vote: Map({
+          pair: List.of('Interstellar', 'cloud atlas'),
+          tally: Map({Interstellar: 1})
+        })
+      })
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Interstellar', 'cloud atlas'],
+        tally: {Interstellar: 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE with plain JS payload', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Interstellar', 'cloud atlas'],
+          tally: {Interstellar: 1}
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Interstellar', 'cloud atlas'],
+        tally: {Interstellar: 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE without initial state', () => {
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Interstellar', 'cloud atlas'],
+          tally: {Interstellar: 1}
+        }
+      }
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Interstellar', 'cloud atlas'],
+        tally: {Interstellar: 1}
+      }
+    }));
+  });
+});
